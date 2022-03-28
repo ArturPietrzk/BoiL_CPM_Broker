c2219da0f9cff39b2c2b01dd8f6e90c7fb5b905e
ArturPietrzk <72975469+ArturPietrzk@users.noreply.github.com>
2022-03-28 12:35:31 +0200

    Update CriticalPath.java

--- boil/src/CriticalPath.java
+++ boil/src/CriticalPath.java
@@ -8,17 +8,29 @@ public class CriticalPath {
     public static void main(String[] args) {
         // The example dependency graph
         HashSet<Task> allTasks = new HashSet<Task>();
-        Task end = new Task("End", 0);
-        Task F = new Task("F", 2, end);
-        Task A = new Task("A", 3, end);
-        Task X = new Task("X", 4, F, A);
-        Task Q = new Task("Q", 2, A, X);
-        Task start = new Task("Start", 0, Q);
+         Task end = new Task("End", 0);
+        Task J = new Task("J", 2, end);
+        Task I = new Task("I", 1, J);
+        Task E = new Task("E", 1, I);
+        Task D = new Task("D", 7, E);
+        Task B = new Task("B", 4, D);
+        Task F = new Task("F", 2, I);
+        Task H = new Task("H", 4, I);
+        Task G = new Task("G", 3, H);
+        Task C = new Task("C", 6, G,F);
+        Task A = new Task("A", 3, B, C);
+        Task start = new Task("Start", 0, A);
         allTasks.add(end);
+        allTasks.add(J);
+        allTasks.add(I);
+        allTasks.add(H);
+        allTasks.add(G);
         allTasks.add(F);
+        allTasks.add(E);
+        allTasks.add(D);
+        allTasks.add(C);
+        allTasks.add(B);
         allTasks.add(A);
-        allTasks.add(X);
-        allTasks.add(Q);
         allTasks.add(start);
         Task[] result = criticalPath(allTasks);
         print(result);
